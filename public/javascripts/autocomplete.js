2eece540f4dd4ec1a4923f4e6ad38ead728cd13d
peixotte <peixotterafael@gmail.com>
2017-10-10 17:24:31 +0200

    API Autocomplete

--- public/javascripts/autocomplete.js
+++ public/javascripts/autocomplete.js
@@ -1,19 +1,32 @@
 $(document).ready(function(){
-    $('input.autocomplete').autocomplete({
-        source: function( request, response ) {
-            $.ajax({
-                url: "https://timetable.search.ch/api/completion.en.json?show_ids=1&nofavorites=1&term="+request.term,
-                dataType: "jsonp",
-                success: function (data) {
-                    alert("ici");
-                    return response(data);
+
+    var stations = {};
+    var stationsString;
+
+    $('input.autocomplete').on('input',function(e){
+        var input = $('input.autocomplete').val();
+        $.getJSON( "https://timetable.search.ch/api/completion.en.json?show_ids=1&nofavorites=1&term="+input, function( data ) {
+
+        }).done(function (data) {
+            $.each(data, function( id, val ) {
+                if (val.iconclass.toLowerCase().indexOf("bus") >= 0)
+                {
+                    stations[val.label] = null;
                 }
             });
-        },
-        limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
-        onAutocomplete: function(val) {
-            // Callback function when value is autcompleted.
-        },
-        minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+
+            $('input.autocomplete').autocomplete({
+                data: stations,
+                limit: 5, // The max amount of results that can be shown at once. Default: Infinity.
+                onAutocomplete: function(val) {
+                    alert(stations);
+                    // Callback function when value is autcompleted.
+                },
+                minLength: 3, // The minimum length of the input for the autocomplete to start. Default: 1.
+                on
+            });
+
+            stationsString = JSON.stringify(stations);
+        });
     });
 });
